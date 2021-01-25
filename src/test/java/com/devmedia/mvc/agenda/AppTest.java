fc8326b8db7b93556a354fa86811f92d3c8a5f3d
remingtonsb <remingtonsb@gmail.com>
2021-01-25 01:14:13 -0300

    Initial commit

---
+++ src/test/java/com/devmedia/mvc/agenda/AppTest.java
@@ -0,0 +1,20 @@
+package com.devmedia.mvc.agenda;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
