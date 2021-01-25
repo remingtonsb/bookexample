4ef62620ead363ef0d55e7661aa5e2aca5e2781e
remingtonsb <remingtonsb@gmail.com>
2021-01-25 01:55:40 -0300

    update manifest

--- src/main/java/com/devmedia/mvc/application/Application.java
+++ src/main/java/com/devmedia/mvc/application/Application.java
@@ -1,4 +1,4 @@
-package com.devmedia.mvc.application;
+ocpackage com.devmedia.mvc.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
